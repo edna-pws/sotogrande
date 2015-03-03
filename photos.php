cba6ea13606e55b827ca3d04d07ac4e28807ea3c
edna-pws <philwebservices.vanessa@gmail.com>
2015-03-03 15:29:45 +0800

    update styles

---
+++ photos.php
@@ -0,0 +1,34 @@
+<?php include_once 'header.php'; ?>
+
+<section class="section skin-gray section-photos">
+	<div class="container">
+		<h2 class="section-title photos-header">photos</h2>
+		<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+		  <!-- Indicators -->
+		  <ol class="carousel-indicators">
+		    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+		    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+		    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+		  </ol>
+
+		  <!-- Wrapper for slides -->
+		  <div class="carousel-inner" role="listbox">
+		    <div class="item active">
+		      <img src="images/img2.jpg" alt="img2">
+		    </div>
+		  </div>
+
+		  <!-- Controls -->
+		  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+		    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+		    <span class="sr-only">Previous</span>
+		  </a>
+		  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+		    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+		    <span class="sr-only">Next</span>
+		  </a>
+</div>
+	</div>
+</section>
+
+<?php include_once 'footer.php'; ?>
--- promos.php
+++ promos.php
@@ -5,57 +5,70 @@
 		<h3 class="banner-subtitle">promos</h3>
 	</div>
 </section>
-<section class="section skin-white section-promos promos-skin">
+<section class="section skin-white section-promos section-bottom-line">
 	<div class="container">
-		<div class="promos">
-			<h2 class="title-reg promos-header">SotoGrand Package Tour Offers</h2>
-			<p class="promos-par">
-				Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis
-				error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi 
-				architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas.
-			</p>
-			<div class="row">
-			  <div class="col-sm-6 col-md-4">
-			    <div class="thumbnail promos-thumbnail">
-			      <img src="images/placeholder/soto-promos_03.jpg" alt="soto-promos_03">
-			      <div class="caption promos-caption">
-			        <h3 class="promos-caption-header">All Inclusive Experience</h3>
-			        <p class="promos-par2">	Experience more at SotoGrande  Hotel &amp; Resort. 
-											3 Days/2 Nights Accommodation + Cebu City Tour
-											Minimum of 2 persons at Php. 7,645.00/person
-					</p>
-			        <p><a href="#" class="btn btn-primary promos-btn" role="button">learn more</a></p>
-			      </div>
-			    </div>
+		<!-- Indicators -->
+		  <ol class="carousel-indicators promos-carousel">
+		    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+		    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+		    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+		  </ol>
+		  <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+
+		  <!-- Wrapper for slides -->
+		  <div class="carousel-inner promos-list" role="listbox">
+		  	<div class="row">
+		    <div class="item active">
+		      <div class="col-sm-6 col-md-4 promos-border">
+					    <div class="thumbnail promos-thumbnail">
+					      <img src="images/placeholder/soto-promos_03.jpg" alt="soto-promos_03">
+					      <div class="caption promos-caption">
+					        <h3 class="promos-caption-header">All Inclusive Experience</h3>
+					        <p class="promos-par2">	Experience more at SotoGrande  Hotel &amp; Resort. 
+													3 Days/2 Nights Accommodation + Cebu City Tour
+													Minimum of 2 persons at Php. 7,645.00/person
+							</p>
+					        <p><a href="#" class="btn btn-primary promos-btn" role="button">learn more</a></p>
+					      </div>
+					    </div>
+					  </div>
+		    </div>
+		    <div class="item">
+		     <div class="col-sm-6 col-md-4 promos-border">
+					    <div class="thumbnail promos-thumbnail">
+					      <img src="images/placeholder/soto-promos_05.jpg" alt="soto-promos_03">
+					      <div class="caption promos-caption">
+					        <h3 class="promos-caption-header">The Queen  City of the South Cebu</h3>
+					        <p class="promos-par2">	Experience more at SotoGrande  Hotel &amp; Resort. 
+													3 Days/2 Nights Accommodation + Cebu City Tour
+													Minimum of 2 persons at Php. 7,645.00/person
+							</p>
+					        <p><a href="#" class="btn btn-primary promos-btn active" role="button">learn more</a></p>
+					      </div>
+					    </div>
+					  </div>
+		    </div>
+			<div class="item">
+			     <div class="col-sm-6 col-md-4 promos-border">
+						    <div class="thumbnail promos-thumbnail">
+						      <img src="images/placeholder/soto-promos_07.jpg" alt="soto-promos_03">
+						      <div class="caption promos-caption">
+						        <h3 class="promos-caption-header">All Inclusive Experience</h3>
+						        <p class="promos-par2">	Experience more at SotoGrande  Hotel &amp; Resort. 
+														3 Days/2 Nights Accommodation + Cebu City Tour
+														Minimum of 2 persons at Php. 7,645.00/person
+								</p>
+						        <p><a href="#" class="btn btn-primary promos-btn" role="button">learn more</a></p>
+						      </div>
+						    </div>
+				  </div>
 			  </div>
-			  <div class="col-sm-6 col-md-4">
-			    <div class="thumbnail promos-thumbnail">
-			      <img src="images/placeholder/soto-promos_05.jpg" alt="soto-promos_03">
-			      <div class="caption promos-caption">
-			        <h3 class="promos-caption-header">The Queen  City of the South Cebu</h3>
-			        <p class="promos-par2">	Experience more at SotoGrande  Hotel &amp; Resort. 
-											3 Days/2 Nights Accommodation + Cebu City Tour
-											Minimum of 2 persons at Php. 7,645.00/person
-					</p>
-			        <p><a href="#" class="btn btn-primary promos-btn" role="button">learn more</a></p>
-			      </div>
-			    </div>
-			  </div>
-			  <div class="col-sm-6 col-md-4">
-			    <div class="thumbnail promos-thumbnail">
-			      <img src="images/placeholder/soto-promos_07.jpg" alt="soto-promos_03">
-			      <div class="caption promos-caption">
-			        <h3 class="promos-caption-header">All Inclusive Experience</h3>
-			        <p class="promos-par2">	Experience more at SotoGrande  Hotel &amp; Resort. 
-											3 Days/2 Nights Accommodation + Cebu City Tour
-											Minimum of 2 persons at Php. 7,645.00/person
-					</p>
-			        <p><a href="#" class="btn btn-primary promos-btn" role="button">learn more</a></p>
-			      </div>
-			    </div>
-			  </div>
-			</div>
-  		</div>
+		    </div>
+		  </div>
+
+		  <!-- Controls -->
+		 
+		</div>
 	</div>
 </section>
 
