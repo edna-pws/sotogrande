a96e6b75e7b9c0d9aec16407184d148ac1e55f19
edna-pws <philwebservices.vanessa@gmail.com>
2015-03-03 17:35:16 +0800

    updated .gap

--- rooms.php
+++ rooms.php
@@ -12,7 +12,7 @@
 			<p class="copy-par1">
 				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
 			</p>
-			<p class="copy-par">			
+			<p class="copy-par2">			
 				Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis
 				error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi 
 				architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas.
@@ -21,19 +21,19 @@
 		<div class="mainrooms-list">
 			<div class="row">
 			  <div class="col-xs-6 col-md-3 mainrooms-max">
-			    <a href="#" class="thumbnail">
+			    <a href="#" class="thumbnail mainrooms-bg">
 			      <img src="images/placeholder/soto-rooms_03.jpg" alt="rooms">
 			      <p class="rooms-caption">classic rooms</p>
 			    </a>
 			  </div>
 			  <div class="col-xs-6 col-md-3 mainrooms-max">
-			    <a href="#" class="thumbnail">
+			    <a href="#" class="thumbnail mainrooms-bg">
 			      <img src="images/placeholder/soto-rooms_05.jpg" alt="rooms">
 			      <p class="rooms-caption">superior rooms</p>
 			    </a>
 			  </div>
 			  <div class="col-xs-6 col-md-3 mainrooms-max">
-			    <a href="#" class="thumbnail">
+			    <a href="#" class="thumbnail mainrooms-bg">
 			      <img src="images/placeholder/soto-rooms_07.jpg" alt="rooms">
 			      <p class="rooms-caption">superior rooms</p>
 			    </a>
@@ -41,19 +41,39 @@
 			</div>
 			<div class="row">
 			  <div class="col-xs-6 col-md-3 mainrooms-max">
-			    <a href="#" class="thumbnail">
+			    <a href="#" class="thumbnail mainrooms-bg">
 			      <img src="images/placeholder/soto-rooms_03.jpg" alt="rooms">
-			      <p class="rooms-caption">classic rooms</p>
+			      <p class="rooms-caption">delux rooms</p>
 			    </a>
 			  </div>
 			  <div class="col-xs-6 col-md-3 mainrooms-max">
-			    <a href="#" class="thumbnail">
+			    <a href="#" class="thumbnail mainrooms-bg">
 			      <img src="images/placeholder/soto-rooms_05.jpg" alt="rooms">
+			      <p class="rooms-caption">ellerman house suites</p>
+			    </a>
+			  </div>
+			  <div class="col-xs-6 col-md-3 mainrooms-max">
+			    <a href="#" class="thumbnail mainrooms-bg">
+			      <img src="images/placeholder/soto-rooms_07.jpg" alt="rooms">
+			      <p class="rooms-caption">delux rooms</p>
+			    </a>
+			  </div>
+			</div>
+			<div class="row">
+			  <div class="col-xs-6 col-md-3 mainrooms-max">
+			    <a href="#" class="thumbnail mainrooms-bg">
+			      <img src="images/placeholder/soto-rooms_03.jpg" alt="rooms">
 			      <p class="rooms-caption">superior rooms</p>
 			    </a>
 			  </div>
 			  <div class="col-xs-6 col-md-3 mainrooms-max">
-			    <a href="#" class="thumbnail">
+			    <a href="#" class="thumbnail mainrooms-bg">
+			      <img src="images/placeholder/soto-rooms_05.jpg" alt="rooms">
+			      <p class="rooms-caption">classic rooms</p>
+			    </a>
+			  </div>
+			  <div class="col-xs-6 col-md-3 mainrooms-max">
+			    <a href="#" class="thumbnail mainrooms-bg">
 			      <img src="images/placeholder/soto-rooms_07.jpg" alt="rooms">
 			      <p class="rooms-caption">superior rooms</p>
 			    </a>
