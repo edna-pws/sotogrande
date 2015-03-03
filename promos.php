050f69d0d769f16d3ae34fde51b4c630b2146589
edna-pws <philwebservices.vanessa@gmail.com>
2015-03-03 09:30:01 +0800

    update image

--- promos.php
+++ promos.php
@@ -8,7 +8,7 @@
 <section class="section skin-white section-promos promos-skin">
 	<div class="container">
 		<div class="promos">
-			<h2 class="section-title">SotoGrand Package Tour Offers</h2>
+			<h2 class="title-reg promos-header">SotoGrand Package Tour Offers</h2>
 			<p class="promos-par">
 				Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis
 				error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi 
@@ -16,12 +16,41 @@
 			</p>
 			<div class="row">
 			  <div class="col-sm-6 col-md-4">
-			    <div class="thumbnail">
-			      <img src="..." alt="...">
-			      <div class="caption">
-			        <h3>Thumbnail label</h3>
-			        <p>...</p>
-			        <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+			    <div class="thumbnail promos-thumbnail">
+			      <img src="images/placeholder/soto-promos_03.jpg" alt="soto-promos_03">
+			      <div class="caption promos-caption">
+			        <h3 class="promos-caption-header">All Inclusive Experience</h3>
+			        <p class="promos-par2">	Experience more at SotoGrande  Hotel &amp; Resort. 
+											3 Days/2 Nights Accommodation + Cebu City Tour
+											Minimum of 2 persons at Php. 7,645.00/person
+					</p>
+			        <p><a href="#" class="btn btn-primary promos-btn" role="button">learn more</a></p>
+			      </div>
+			    </div>
+			  </div>
+			  <div class="col-sm-6 col-md-4">
+			    <div class="thumbnail promos-thumbnail">
+			      <img src="images/placeholder/soto-promos_05.jpg" alt="soto-promos_03">
+			      <div class="caption promos-caption">
+			        <h3 class="promos-caption-header">The Queen  City of the South Cebu</h3>
+			        <p class="promos-par2">	Experience more at SotoGrande  Hotel &amp; Resort. 
+											3 Days/2 Nights Accommodation + Cebu City Tour
+											Minimum of 2 persons at Php. 7,645.00/person
+					</p>
+			        <p><a href="#" class="btn btn-primary promos-btn" role="button">learn more</a></p>
+			      </div>
+			    </div>
+			  </div>
+			  <div class="col-sm-6 col-md-4">
+			    <div class="thumbnail promos-thumbnail">
+			      <img src="images/placeholder/soto-promos_07.jpg" alt="soto-promos_03">
+			      <div class="caption promos-caption">
+			        <h3 class="promos-caption-header">All Inclusive Experience</h3>
+			        <p class="promos-par2">	Experience more at SotoGrande  Hotel &amp; Resort. 
+											3 Days/2 Nights Accommodation + Cebu City Tour
+											Minimum of 2 persons at Php. 7,645.00/person
+					</p>
+			        <p><a href="#" class="btn btn-primary promos-btn" role="button">learn more</a></p>
 			      </div>
 			    </div>
 			  </div>
