1773c805ea4f3686d526646a82f59679c30b3833
Marvin Costal <philwebservices.costal@gmail.com>
2015-03-04 11:43:18 +0800

    added owl carousel

--- footer.php
+++ footer.php
@@ -1,86 +1,79 @@
-<section class="section section-footer">
-	<div class="container-lg">
-		<div class="row">
-			<div class="col-sm-12 col-md-4">
-				<div class="footer-location">
-					<h4 class="footer-title">SotoGrande</h4>
-					<p>Dapdap, Mactan Island, Lapu-Lapu City,</p>
-					<p>Cebu, Philippines</p>
+	<section class="section section-footer">
+		<div class="container-lg">
+			<div class="row">
+				<div class="col-sm-12 col-md-4">
+					<div class="footer-location">
+						<h4 class="footer-title">SotoGrande</h4>
+						<p>Dapdap, Mactan Island, Lapu-Lapu City,</p>
+						<p>Cebu, Philippines</p>
+					</div>
 				</div>
-			</div>
 
-			<div class="col-sm-12 col-md-8">
-				<div class="footer-connect">
-					<h3 class="title-bold text-right">Stay Connected</h3>
-					<ul class="social">
-						<li><a href="/"><span><i class="fa fa-facebook"></i></span></a></li>
-						<li><a href="/"><span><i class="fa fa-twitter"></i></span></a></li>
-						<li><a href="/"><span><i class="fa fa-linkedin"></i></span></a></li>
-						<li><a href="/"><span><i class="fa fa-google-plus"></i></span></a></li>
-					</ul>
+				<div class="col-sm-12 col-md-8">
+					<div class="footer-connect">
+						<h3 class="title-bold text-right">Stay Connected</h3>
+						<ul class="social">
+							<li><a href="/"><span><i class="fa fa-facebook"></i></span></a></li>
+							<li><a href="/"><span><i class="fa fa-twitter"></i></span></a></li>
+							<li><a href="/"><span><i class="fa fa-linkedin"></i></span></a></li>
+							<li><a href="/"><span><i class="fa fa-google-plus"></i></span></a></li>
+						</ul>
+					</div>
 				</div>
-			</div>
-			<div class="col-md-12 p0 mt10">
-				<div class="col-md-6">
-					<p>Phone: +63032 238 6590</p>
-				</div>
-				<div class="col-md-6 pr0">
-					<ul class="nav navbar-nav m0">
-				        <li><a href="#">about</a></li>
-				        <li><a href="#">properties</a></li>
-				        <li><a href="#">sitemap</a></li>
-				        <li><a href="#">subscribe newsletter</a></li>
-				        <li><a href="#">contact</a></li>
-				     </ul>
+				<div class="col-md-12 p0 mt10">
+					<div class="col-md-6">
+						<p>Phone: +63032 238 6590</p>
+					</div>
+					<div class="col-md-6 pr0">
+						<ul class="nav navbar-nav m0">
+					        <li><a href="#">about</a></li>
+					        <li><a href="#">properties</a></li>
+					        <li><a href="#">sitemap</a></li>
+					        <li><a href="#">subscribe newsletter</a></li>
+					        <li><a href="#">contact</a></li>
+					     </ul>
+					</div>
 				</div>
 			</div>
 		</div>
-	</div>
-</section>
+	</section>
 
-<footer>
-	<div class="container-lg clearfix">
-		<div class="pull-left">
-			<img src="images/icons/logo-xs.png" alt=""> © SotoGrande <?php echo date('Y'); ?>. All rights reserved. Privacy / Contact Us
+	<footer>
+		<div class="container-lg clearfix">
+			<div class="pull-left">
+				<img src="images/icons/logo-xs.png" alt=""> © SotoGrande <?php echo date('Y'); ?>. All rights reserved. Privacy / Contact Us
+			</div>
+
+			<div class="pull-right">
+				designed and developed by: philwebservices.
+			</div>
 		</div>
+	</footer>
+	<script src="scripts/vendor/jquery-2.1.1.js"></script>
+	<script src='https://maps.googleapis.com/maps/api/js?key=&sensor=false&extension=.js'></script> 
+	<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?sensor=false&region=US"></script>
 
-		<div class="pull-right">
-			designed and developed by: philwebservices.
-		</div>
-	</div>
-</footer>
+	<!-- <script src="scripts/bootstrap.min.js"></script>
 
+	<script src="scripts/jquery.bxslider.js"></script>
+
+	<script src="scripts/jquery.easing.1.3.js"></script>
+
+	<script src="scripts/jquery.fitvids.js"></script>	
+
+	<script src="scripts/min/jquery.bxslider.min.js"></script>
+	 -->
+	<script src="scripts/bootstrap-datepicker.js"></script>
+	<script src="scripts/main.js"></script>
+	<script src="assets/owl-carousel/owl.carousel.js"></script>
+	<script>
+		$(function(){
+			$('#dp3').datepicker({
+				format: 'mm-dd-yyyy'
+			});
+		});
+	</script>
 </body>
-
-<script>
-
-$(function(){
-	$('#dp3').datepicker({
-		format: 'mm-dd-yyyy'
-	});
-});
-
-</script>
-
-<script src='https://maps.googleapis.com/maps/api/js?key=&sensor=false&extension=.js'></script> 
-<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?sensor=false&region=US"></script>
-
-<script src="scripts/vendor/jquery-2.1.1.js"></script>
-
-<!-- <script src="scripts/bootstrap.min.js"></script>
-
-<script src="scripts/jquery.bxslider.js"></script>
-
-<script src="scripts/jquery.easing.1.3.js"></script>
-
-<script src="scripts/jquery.fitvids.js"></script>	
-
-<script src="scripts/min/jquery.bxslider.min.js"></script>
- -->
-<script src="scripts/bootstrap-datepicker.js"></script>
-
-<script src="scripts/main.js"></script>
-
 </html>
 
 
