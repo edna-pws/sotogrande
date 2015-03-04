1c03d29d88714d57e53b96e4ce737471dafdf1d7
edna-pws <philwebservices.vanessa@gmail.com>
2015-03-04 08:24:32 +0800

    rooms

--- rooms.php
+++ rooms.php
@@ -9,7 +9,6 @@
 	<div class="container">
 		<div class="section-mainrooms-copy">
 			<p class="copy-par1">
-			FJHFJHnkfnf
 				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
 			</p>
 			<p class="copy-par2">			
