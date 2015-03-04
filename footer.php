551f32d30e69e82836312a7a313db9f6e404f047
Marvin Costal <philwebservices.costal@gmail.com>
2015-03-04 11:21:19 +0800

    fixed footer

--- footer.php
+++ footer.php
@@ -1,12 +1,11 @@
 <section class="section section-footer">
-	<div class="container-fluid container-lg">
+	<div class="container-lg">
 		<div class="row">
 			<div class="col-sm-12 col-md-4">
 				<div class="footer-location">
 					<h4 class="footer-title">SotoGrande</h4>
 					<p>Dapdap, Mactan Island, Lapu-Lapu City,</p>
 					<p>Cebu, Philippines</p>
-					<p>Phone: +63032 238 6590</p>
 				</div>
 			</div>
 
@@ -21,23 +20,28 @@
 					</ul>
 				</div>
 			</div>
-			<div class="col-sm-12 col-md-12">
-				<ul class="nav navbar-nav">
-			        <li><a href="#">about</a></li>
-			        <li><a href="#">properties</a></li>
-			        <li><a href="#">sitemap</a></li>
-			        <li><a href="#">subscribe newsletter</a></li>
-			        <li><a href="#">contact</a></li>
-			      </ul>
+			<div class="col-md-12 p0 mt10">
+				<div class="col-md-6">
+					<p>Phone: +63032 238 6590</p>
+				</div>
+				<div class="col-md-6 pr0">
+					<ul class="nav navbar-nav m0">
+				        <li><a href="#">about</a></li>
+				        <li><a href="#">properties</a></li>
+				        <li><a href="#">sitemap</a></li>
+				        <li><a href="#">subscribe newsletter</a></li>
+				        <li><a href="#">contact</a></li>
+				     </ul>
+				</div>
 			</div>
 		</div>
 	</div>
 </section>
 
 <footer>
-	<div class="container">
+	<div class="container-lg clearfix">
 		<div class="pull-left">
-			<img src="images/icons/logo-xs.png" alt="">© SotoGrande 2015. All rights reserved. Privacy / Contact Us
+			<img src="images/icons/logo-xs.png" alt=""> © SotoGrande <?php echo date('Y'); ?>. All rights reserved. Privacy / Contact Us
 		</div>
 
 		<div class="pull-right">
