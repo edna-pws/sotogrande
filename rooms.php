c3696d20fb4072721a10e337b9a128eb640dd9ff
edna-pws <philwebservices.vanessa@gmail.com>
2015-03-04 08:20:47 +0800

    edit: rooms

--- rooms.php
+++ rooms.php
@@ -9,7 +9,7 @@
 	<div class="container">
 		<div class="section-mainrooms-copy">
 			<p class="copy-par1">
-			FJHFJH
+			FJHFJHnkfnf
 				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
 			</p>
 			<p class="copy-par2">			
