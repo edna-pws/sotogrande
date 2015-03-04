8f6d448199d5e70bf131afcc8d4fcbc24bd30a3f
edna-pws <philwebservices.vanessa@gmail.com>
2015-03-04 13:45:01 +0800

    updated: photos

--- photos.php
+++ photos.php
@@ -1,45 +1,95 @@
 <?php include_once 'header.php'; ?>
 
 <section class="section skin-gray skin-gap section-photos">
-	<div class="container">
+	<div class="container bottom-gap">
 		<h2 class="section-title photos-header">photos</h2>
 		<div class="gap gap-3em"></div>
-		<section class="section-carousel l-banner">
-			<div id="banner-slider" class="carousel slide banner-slider banner-slider-md" data-ride="carousel">
+	<div class="container section-photos-banner">
+  
+    <!-- main slider carousel -->
+    <div class="row">
+        <div class="col-md-12" id="slider">
+            
+                <div class="col-md-12" id="carousel-bounding-box">
+                    <div id="myCarousel" class="carousel slide">
+                        <!-- main slider carousel items -->
+                        <div class="carousel-inner">
+                            <div class="active item section-photos-img-border" data-slide-number="0">
+                                <img src="images/soto-photos-lg_03.jpg" class="img-responsive">
+                            </div>
+                            <div class="item" data-slide-number="1">
+                                <img src="images/soto-photos-lg_03.jpg" class="img-responsive">
+                            </div>
+                            <div class="item" data-slide-number="2">
+                                <img src="" class="img-responsive">
+                            </div>
+                            <div class="item" data-slide-number="3">
+                                <img src="" class="img-responsive">
+                            </div>
+                            <div class="item" data-slide-number="4">
+                                <img src="" class="img-responsive">
+                            </div>
+                            <div class="item" data-slide-number="5">
+                                <img src="" class="img-responsive">
+                            </div>
+                            <div class="item" data-slide-number="6">
+                                <img src="" class="img-responsive">
+                            </div>
+                            <div class="item" data-slide-number="7">
+                                <img src="" class="img-responsive">
+                            </div>
+                        </div>
+                        <!-- main slider carousel nav controls -->
+                        <a class="carousel-control left photos-control-left" href="#myCarousel" data-slide="prev">‹</a>
 
-			<ol class="carousel-indicators">
-			    <li data-target="#banner-slider" data-slide-to="0" class="active"></li>
-			    <li data-target="#banner-slider" data-slide-to="1"></li>
-			    <li data-target="#banner-slider" data-slide-to="2"></li>
-			  </ol>
+                        <a class="carousel-control right photos-control-right" href="#myCarousel" data-slide="next">›</a>
+                    </div>
+                </div>
 
-			  <div class="carousel-inner" role="listbox">
-			    <div class="item active">
-			      <div class="banner-img">
-			      	<img src="images/soto-photos-lg_03.jpg" alt="...">
-			      </div>
-			    </div>
+        </div>
+    </div>
+    <!--/main slider carousel-->
+    <!-- thumb navigation carousel -->
+    <div class="row section-photos-row">
+        <div class="col-md-12 hidden-sm hidden-xs photos-indicator-border" id="slider-thumbs">
+            
+                <!-- thumb navigation carousel items -->
+              <ul class="list-inline">
+                  <li class="photos-no-pad"> <a id="carousel-selector-0" class="selected">
+                    <img src="images/placeholder/soto-photos-small_03.jpg" class="img-responsive">
+                  </a></li>
+                  <li class="photos-no-pad"> <a id="carousel-selector-1">
+                    <img src="images/placeholder/soto-photos-small_04.jpg" class="img-responsive">
+                  </a></li>
+                  <li class="photos-no-pad"> <a id="carousel-selector-2">
+                    <img src="images/placeholder/soto-photos-small_05.jpg" class="img-responsive">
+                  </a></li>
+                  <li class="photos-no-pad"> <a id="carousel-selector-3">
+                    <img src="images/placeholder/soto-photos-small_06.jpg" class="img-responsive">
+                  </a></li>
+                   <li class="photos-no-pad"> <a id="carousel-selector-4">
+                    <img src="images/placeholder/soto-photos-small_07.jpg" class="img-responsive">
+                  </a></li>
+                  <li class="photos-no-pad"> <a id="carousel-selector-5">
+                    <img src="images/placeholder/soto-photos-small_08.jpg" class="img-responsive">
+                  </a></li>
+                  <li class="photos-no-pad"> <a id="carousel-selector-6">
+                    <img src="images/placeholder/soto-photos-small_09.jpg" class="img-responsive">
+                  </a></li>
+                </ul>
+            
+        </div>
+    </div>
+	
+	<div class="section-photo-par">
+    	<p>
+    		Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis
+    		error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis
+    	et quasi architecto beatae vitae dicta sunt explicabo.
+    	</p>
+	</div>
 
-			    <div class="item">
-			      <div class="banner-img">
-			      	<img src="images/banner.jpg" alt="...">
-			      </div>
-
-			    </div>
-
-			  </div>
-
-			 <a class="left carousel-control photos-control" href="#banner-slider" role="button" data-slide="prev">
-			    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-			    <span class="sr-only">Previous</span>
-			  </a>
-			  <a class="right carousel-control photos-control-right" href="#banner-slider" role="button" data-slide="next">
-			    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-			    <span class="sr-only">Next</span>
-			  </a> 
-				</div>
-		</section>
+	</div>
 	</div>
 </section>
-
 <?php include_once 'footer.php'; ?>
