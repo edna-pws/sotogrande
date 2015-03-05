3ec076b6d6bd4cd6651ce2ee93a696adf08465bd
edna-pws <philwebservices.vanessa@gmail.com>
2015-03-05 08:25:27 +0800

    done

--- index.php
+++ index.php
@@ -45,11 +45,11 @@
 	  </div>
 
 
-	  <a class="left carousel-control photos-control-left home-slider1" href="#banner-slider" role="button" data-slide="prev">
+	  <a class="left carousel-control home-slider1 photos-control-left" href="#banner-slider" role="button" data-slide="prev">
 	    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
 	    <span class="sr-only">Previous</span>
 	  </a>
-	  <a class="right carousel-control photos-control-right home-slider1" href="#banner-slider" role="button" data-slide="next">
+	  <a class="right carousel-control home-slider2 photos-control-right" href="#banner-slider" role="button" data-slide="next">
 	    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
 	    <span class="sr-only">Next</span>
 	  </a>
