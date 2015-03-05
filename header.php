9e4d9d139a51de134f386fca6eabb705e338ca4f
Marvin Costal <philwebservices.costal@gmail.com>
2015-03-05 18:06:18 +0800

    fixed navbar

--- header.php
+++ header.php
@@ -10,7 +10,6 @@
       <title>Soto Grande Hotel | </title>
       <meta name="description" content="">
       <meta name="viewport" content="width=device-width, initial-scale=1">
-      <link rel="stylesheet" href="styles/bootstrap.min.css">
       <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
       <link rel="stylesheet" href="owl-carousel/owl.carousel.css">
       <link rel="stylesheet" href="owl-carousel/owl.theme.css">
